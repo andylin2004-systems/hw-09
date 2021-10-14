15ba8fe6be0f29506fb7d828518c0a845ffce734
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2021-10-14 18:02:21 -0400

    name should've been a pointer, allocpokemon func shouldve returned a pointer, should be deref in print pokemon

--- hw-09.c
+++ hw-09.c
@@ -1,25 +1,24 @@
+#include <stdlib.h>
 #include <stdio.h>
 
 struct pokemon {
-    char name;
+    char *name;
     int hp;
 };
 
-void printPokemon(struct pokemon x){
-    printf("This Pokemon is %s, with a max HP of %d", x.name, x.hp);
+void printPokemon(struct pokemon * x){
+    printf("This Pokemon is %s, with a max HP of %d\n", x->name, x->hp);
 }
 
-struct pokemon allocPokemon(char name, int hp){
+struct pokemon * allocPokemon(char *name, int hp){
     struct pokemon *p = malloc(sizeof(struct pokemon));
     p->name = name;
     p->hp = hp;
-    return *p;
+    return p;
 }
 
 int main(){
-    int i;
-    for (i = 0; i < 26; i++){
-        printPokemon(allocPokemon(i, i));
-    }
+    struct pokemon *p = allocPokemon("random", 1);
+    printPokemon(p);
     return 0;
 }
